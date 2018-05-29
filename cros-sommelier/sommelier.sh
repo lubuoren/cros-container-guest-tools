2b9228bb88b1f925a7e41afe8e153277498134a0
Jeffrey Kardatzke <jkardatzke@google.com>
2018-05-29 16:05:12 -0700

    Add WAYLAND_DISPLAY env var for sommelier
    BUG=chromium:828703
    TEST=Launching terminator in terminal/launcher works
    
    Change-Id: Ied28b2ac7ec44bab3f1392e4c2f10181f3111993
    Reviewed-on: https://chromium-review.googlesource.com/1077608
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>
    Tested-by: Jeffrey Kardatzke <jkardatzke@google.com>

--- cros-sommelier/sommelier.sh
+++ cros-sommelier/sommelier.sh
@@ -1,11 +1,20 @@
 # Not bash or zsh?
 [ -n "$BASH_VERSION" -o -n "$ZSH_VERSION" ] || return 0
 
-# DISPLAY already set?
-[ -z "$DISPLAY" ] || return 0
-
 # Not running under sommelier?
 [ $(systemctl --user show-environment | grep ^SOMMELIER_VERSION=) ] || return 0
 
-export $(systemctl --user show-environment | grep ^DISPLAY=)
-export $(systemctl --user show-environment | grep ^XCURSOR_SIZE=)
+# DISPLAY not set?
+if [ -n "$DISPLAY" ]; then
+  export $(systemctl --user show-environment | grep ^DISPLAY=)
+fi
+
+# XCURSOR_SIZE not set?
+if [ -n "$XCURSOR_SIZE" ]; then
+  export $(systemctl --user show-environment | grep ^XCURSOR_SIZE=)
+fi
+
+# WAYLAND_DISPLAY not set?
+if [ -n "$WAYLAND_DISPLAY" ]; then
+  export $(systemctl --user show-environment | grep ^WAYLAND_DISPLAY=)
+fi
