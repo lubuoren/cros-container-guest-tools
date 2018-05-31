85c0890041d1bc575cff42076c6a1329bebee098
David Reveman <reveman@chromium.org>
2018-05-31 12:11:09 +0000

    Set DISPLAY, XCURSOR_SIZE and WAYLAND_DISPLAY if not set.
    BUG=chromium:828703
    TEST=They are set after launching terminal
    
    Change-Id: I5d2f7bd6201afb5e93e4b964c58d3f72c2ac3dd0
    Reviewed-on: https://chromium-review.googlesource.com/1079843
    Tested-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Jeffrey Kardatzke <jkardatzke@google.com>

--- cros-sommelier/sommelier.sh
+++ cros-sommelier/sommelier.sh
@@ -5,16 +5,16 @@
 [ $(systemctl --user show-environment | grep ^SOMMELIER_VERSION=) ] || return 0
 
 # DISPLAY not set?
-if [ -n "$DISPLAY" ]; then
+if [ -z "$DISPLAY" ]; then
   export $(systemctl --user show-environment | grep ^DISPLAY=)
 fi
 
 # XCURSOR_SIZE not set?
-if [ -n "$XCURSOR_SIZE" ]; then
+if [ -z "$XCURSOR_SIZE" ]; then
   export $(systemctl --user show-environment | grep ^XCURSOR_SIZE=)
 fi
 
 # WAYLAND_DISPLAY not set?
-if [ -n "$WAYLAND_DISPLAY" ]; then
+if [ -z "$WAYLAND_DISPLAY" ]; then
   export $(systemctl --user show-environment | grep ^WAYLAND_DISPLAY=)
 fi
