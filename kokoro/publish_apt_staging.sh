218b7e485ebb9a7c487298877f677507e248f99b
Stephen Barber <smbarber@chromium.org>
2018-08-22 13:48:58 -0700

    kokoro: add publish stages for apt/lxd
    BUG=none
    TEST=kokoro
    
    Change-Id: I34dbceb9ea2f195c956141804f1ecd2bc8845fa4
    Reviewed-on: https://chromium-review.googlesource.com/1185852
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Commit-Queue: Stephen Barber <smbarber@chromium.org>
    Tested-by: Stephen Barber <smbarber@chromium.org>

---
+++ kokoro/publish_apt_staging.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Copyright 2018 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -ex
+
+. "common.sh" || exit 1
+
+main() {
+    if [ -z "${KOKORO_ARTIFACTS_DIR}" ]; then
+        echo "This script must be run in kokoro"
+        exit 1
+    fi
+
+    if [ -z "${CROS_MILESTONE}" ]; then
+        echo "CROS_MILESTONE must be set"
+        exit 1
+    fi
+
+    local src_root="${KOKORO_ARTIFACTS_DIR}"/git/cros-container-guest-tools
+    local result_dir="${src_root}"/apt
+    if [ "${CROS_MILESTONE}" -ge 70 ]; then
+        result_dir="${result_dir}/${CROS_MILESTONE}"
+    fi
+    mkdir -p "${result_dir}"
+
+    cp -r "${KOKORO_GFILE_DIR}"/apt_signed/* "${result_dir}"
+}
+
+main "$@"
