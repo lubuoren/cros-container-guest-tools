e6d7fd369cba63fa739900946f8ca7fb5617f911
Tim Zheng <timzheng@google.com>
2018-10-10 17:21:54 -0700

    Add environment variable XCURSOR_SIZE_LOW_DENSITY.
    Now that we have a high density and a low density instances of X
    Sommelier, we need to have seperate environment variables for the
    XCURSOR_SIZE.
    
    BUG=chromium:894276
    TEST=Manually tested on an eve device.
    
    Change-Id: Ie07a784cc7805a00de164c9a5738fdcae8db9c5a
    Reviewed-on: https://chromium-review.googlesource.com/1275086
    Commit-Ready: Tim Zheng <timzheng@chromium.org>
    Tested-by: Tim Zheng <timzheng@chromium.org>
    Reviewed-by: Stephen Barber <smbarber@chromium.org>

--- cros-sommelier/sommelier.sh
+++ cros-sommelier/sommelier.sh
@@ -6,26 +6,34 @@
 
 # DISPLAY not set?
 if [ -z "$DISPLAY" ]; then
-  export $(systemctl --user show-environment | grep ^DISPLAY=)
+  export $(systemctl --user show-environment | grep ^DISPLAY=) > /dev/null
 fi
 
 # DISPLAY_LOW_DENSITY not set?
 if [ -z "$DISPLAY_LOW_DENSITY" ]; then
-  export $(systemctl --user show-environment | grep ^DISPLAY_LOW_DENSITY=)
+  export $(systemctl --user show-environment | \
+      grep ^DISPLAY_LOW_DENSITY=) > /dev/null
 fi
 
 # XCURSOR_SIZE not set?
 if [ -z "$XCURSOR_SIZE" ]; then
-  export $(systemctl --user show-environment | grep ^XCURSOR_SIZE=)
+  export $(systemctl --user show-environment | grep ^XCURSOR_SIZE=) > /dev/null
+fi
+
+# XCURSOR_SIZE_LOW_DENSITY not set?
+if [ -z "$XCURSOR_SIZE_LOW_DENSITY" ]; then
+  export $(systemctl --user show-environment | \
+      grep ^XCURSOR_SIZE_LOW_DENSITY=) > /dev/null
 fi
 
 # WAYLAND_DISPLAY not set?
 if [ -z "$WAYLAND_DISPLAY" ]; then
-  export $(systemctl --user show-environment | grep ^WAYLAND_DISPLAY=)
+  export $(systemctl --user show-environment | \
+    grep ^WAYLAND_DISPLAY=) > /dev/null
 fi
 
 # WAYLAND_DISPLAY_LOW_DENSITY not set?
 if [ -z "$WAYLAND_DISPLAY_LOW_DENSITY" ]; then
   export $(systemctl --user show-environment | \
-      grep ^WAYLAND_DISPLAY_LOW_DENSITY=)
+      grep ^WAYLAND_DISPLAY_LOW_DENSITY=) > /dev/null
 fi
