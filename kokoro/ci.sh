fde051f63471c049c3bbde68c1d6cb5d02c37f83
Nicholas Hollingum <hollingum@google.com>
2020-01-17 11:48:49 +1100

    container-guest-tools: Define a CI job to act as the entrypoint.
    The kokoro CI is currently disabled (cl/286950668) however since the fix
    for b/146575733 is not forthcoming, we will instead implement the
    workaround.
    
    We create an empty no-op job script and configuration file to be the new
    head of the build. Once this lands, we can add a step to the kokoro
    configuration in piper to use the "ci" job as the head.
    
    BUG=b:146575733
    TEST=none
    
    Change-Id: Ieadf63ace38d5b3459d2ca5f4fa51439efc839bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/containers/cros-container-guest-tools/+/2006628
    Reviewed-by: Stephen Barber <smbarber@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Tested-by: Nic Hollingum <hollingum@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>

---
+++ kokoro/ci.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -ex
+
+. "$(dirname "$0")/common.sh" || exit 1
+
+# TODO(b/146575733): Currently there is a bug where kokoro jobs that have a
+# group_spec as the initial trigger will fire continuously. In order to
+# work around that bug, we will define a new no-op job to act as the head,
+# whose child will be that group_spec job.
+#
+# Once the bug is fixed, we can go back to using the group_spec job as the
+# trigger.
+main() {
+  # Print the date because why not.
+  date
+}
+
+main "$@"
