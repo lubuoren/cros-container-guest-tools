0a308a68216e7a362466f7a69b9c192e7dc4b453
James Ye <jamesye@google.com>
2022-01-28 12:09:57 +1100

    lxd: Create squashfs images again
    Distrobuilder will build squashfs images, so restore the logic to handle
    them.
    This reverts commit 40571e1c0ca4b27c0149a864deaffc605c012b1a.
    
    BUG=none
    TEST=kokoro
    
    Change-Id: I44cb7332d179ea2ecef7090b38c55e22fe5813e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/containers/cros-container-guest-tools/+/3422357
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Tested-by: kokoro <noreply+kokoro@google.com>
    Commit-Queue: James Ye <jamesye@google.com>

--- lxd/build_debian_container.sh
+++ lxd/build_debian_container.sh
@@ -115,9 +115,10 @@ build_and_export() {
     unmount_all "${rootfs}"
     rm -rf "${rootfs}/opt/google"
 
-    # Repack into 2 tarballs for distribution via simplestreams.
+    # Repack into 2 tarballs + squashfs for distribution via simplestreams.
     # Combined sha256 is lxd.tar.xz | rootfs.
     # rootfs.tar.xz is raw rootfs tar'd up.
+    # rootfs.squashfs is raw rootfs squash'd.
     # lxd.tar.xz is metadata.yaml and templates dir.
     local result_dir="${results_dir}/debian/${release}/${arch}"
     if [ "${image_type}" = "test" ]; then
@@ -133,6 +134,8 @@ build_and_export() {
     local rootfs_tarball="${result_dir}/rootfs.tar.xz"
     cp "${tempdir}/image" "${metadata_tarball}"
     tar -Ipixz --xattrs --acls -cpf "${rootfs_tarball}" -C "${rootfs}" .
+    mksquashfs "${rootfs}" "${result_dir}/rootfs.squashfs" \
+        -noappend -comp xz -b 1M -no-progress -no-recovery
 
     if [ "${arch}" = "amd64" ] && [ "${image_type}" == "prod" ]; then
         # Workaround the "Invalid multipart image" flake by generating a
