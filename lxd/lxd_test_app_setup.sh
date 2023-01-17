7540b9bed277dc464c85b7add98415be834afc17
James Ye <jamesye@google.com>
2023-01-17 14:09:32 +1100

    lxd: Install Visual Studio Code in ARM64 app test images
    Enable testing of VS code on ARM devices.
    
    BUG=b:265720354
    TEST=kokoro
    
    Change-Id: I5bf6a2e987884a0a7cd6b11b473905e7aa12763a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/containers/cros-container-guest-tools/+/4167873
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Reviewed-by: Alvin Jia <alvinjia@google.com>
    Commit-Queue: James Ye <jamesye@google.com>

--- lxd/lxd_test_app_setup.sh
+++ lxd/lxd_test_app_setup.sh
@@ -11,7 +11,9 @@ set -eux -o pipefail
 # To update this, run apt-cache policy code on your DUT with this
 # container installed, and look at the latest version.
 # Note that updating this package is likely to break screendiffs.
-VSCODE_VERSION="1.63.0-1638855526"
+# Releases for different architectures have different build numbers.
+VSCODE_VERSION_AMD64="1.63.0-1638855526"
+VSCODE_VERSION_ARM64="1.63.0-1638854446"
 
 main() {
     local arch=$1
@@ -24,6 +26,13 @@ main() {
         firefox-esr
         gedit
     )
+
+    # for testing Visual Studio Code.
+    curl -sSL https://packages.microsoft.com/keys/microsoft.asc \
+        | gpg --dearmor > /etc/apt/trusted.gpg.d/packages.microsoft.gpg
+    cat > /etc/apt/sources.list.d/vscode.list << EOF
+deb [arch=amd64,arm64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main
+EOF
 
     if [ "${arch}" = "amd64" ]; then
         # for testing Android Studio.
@@ -38,16 +47,12 @@ main() {
         ln -s /usr/eclipse/eclipse /usr/bin/eclipse
         rm -f eclipse.tar.gz
 
-        # for testing Visual Studio Code.
-        curl -sSL https://packages.microsoft.com/keys/microsoft.asc \
-            | gpg --dearmor > /etc/apt/trusted.gpg.d/packages.microsoft.gpg
-        cat > /etc/apt/sources.list.d/vscode.list << EOF
-deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main
-EOF
-        apt-get -q update
-        packages+=( "code=${VSCODE_VERSION}" )
+        packages+=( "code=${VSCODE_VERSION_AMD64}" )
+    elif [ "${arch}" = "arm64" ]; then
+        packages+=( "code=${VSCODE_VERSION_ARM64}" )
     fi
 
+    apt-get -q update
     apt-get -q -y install "${packages[@]}"
 }
 
